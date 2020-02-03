82caa90b64deba604c5b8b96ce42e52fdd07de19
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-03 12:53:23 +0000

    add main to print list of sorted imports to command line

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,10 @@ import java.io.File
 
 fun main(args: Array<String>) {
     println(args.get(0))
+    val fileName = args[0]
+    val imps = fileToList(fileName)
+    val sortedImps = imps.map { getImport(it) }.sortImports()
+    println(sortedImps)
 }
 
 const val delimiter = "import "
@@ -11,4 +15,4 @@ fun fileToList(fileName: String): List<String>
 
 fun getImport(imp: String): String = imp.split(delimiter)[1]
 
-fun sortImports(im: List<String>): List<String> = im.sorted()
+fun List<String>.sortImports(): List<String> = this.sorted()
