236f8134831ba0dd221724a6422e15197d420989
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-03 13:00:29 +0000

    add function to restore import and print back to file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,9 @@ fun main(args: Array<String>) {
     val fileName = args[0]
     val imps = fileToList(fileName)
     val sortedImps = imps.map { getImport(it) }.sortImports()
+    val restoredImp = sortedImps.map { restoreImport(it) }
     println(sortedImps)
+    printBackToFile(fileName, restoredImp)
 }
 
 const val delimiter = "import "
@@ -16,3 +18,7 @@ fun fileToList(fileName: String): List<String>
 fun getImport(imp: String): String = imp.split(delimiter)[1]
 
 fun List<String>.sortImports(): List<String> = this.sorted()
+
+fun restoreImport(imp: String): String = delimiter + imp
+
+fun printBackToFile(fileName: String, imps: List<String>): Unit = File(fileName).writeText(imps.reduce { a, b -> a + "\n" + b})
