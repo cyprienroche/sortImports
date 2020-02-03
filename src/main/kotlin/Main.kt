4eec929e7efab969cba0ad20815525aafb3f99b9
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-03 13:31:32 +0000

    remove redundant function calls from main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,9 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    println(args.get(0))
     val fileName = args[0]
-    val imps = fileToList(fileName)
-    val sortedImps = imps.map { getImport(it) }.sortImports()
-    val restoredImp = sortedImps.map { restoreImport(it) }
-    printBackToFile(fileName, restoredImp)
+    val imps = fileToList(fileName).sorted()
+    printBackToFile(fileName, imps)
 }
 
 const val delimiter = "import "
@@ -17,7 +14,5 @@ fun fileToList(fileName: String): List<String>
 fun getImport(imp: String): String = imp.split(delimiter)[1]
 
 fun List<String>.sortImports(): List<String> = this.sorted()
-
-fun restoreImport(imp: String): String = delimiter + imp
 
 fun printBackToFile(fileName: String, imps: List<String>): Unit = File(fileName).writeText(imps.reduce { a, b -> a + "\n" + b})
