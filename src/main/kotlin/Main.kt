c7b9d1c0a3bdbb7c0847e563f622ea2c74b6544b
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-03 20:46:55 +0000

    make main sort imports even if file contains other info

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
 import java.io.File
+import java.io.BufferedInputStream
+import java.math.BigDecimal
 
 fun main(args: Array<String>) {
     val file = File(args[0])
-    file.writeText(file.readLines().sorted().reduce { a, b -> a + "\n" + b})
+    val partition = file.readLines().partition { it.startsWith("import") }
+    val sortedImports = partition.first.sorted() + partition.second
+    file.writeText(sortedImports.reduce { a, b -> a + "\n" + b})
 }
