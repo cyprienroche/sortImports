a42ab65dda7f08eb1b5e9fa9664a6a476c043866
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-03 13:32:45 +0000

    remove tests no longer needed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,13 +6,7 @@ fun main(args: Array<String>) {
     printBackToFile(fileName, imps)
 }
 
-const val delimiter = "import "
-
 fun fileToList(fileName: String): List<String>
         = File(fileName).readLines()
-
-fun getImport(imp: String): String = imp.split(delimiter)[1]
-
-fun List<String>.sortImports(): List<String> = this.sorted()
 
 fun printBackToFile(fileName: String, imps: List<String>): Unit = File(fileName).writeText(imps.reduce { a, b -> a + "\n" + b})
